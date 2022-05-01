2d5c876f789a33a4c2e9ed51a0327b13127ee2a2
Barabashka <vedeninvv@mail.ru>
2022-05-01 17:33:51 +0300

    lab6 fix: add account name to menu

--- public/js/userInfo.js
+++ public/js/userInfo.js
@@ -1,11 +1,14 @@
 async function userInfo() {
-  supertokens.init({
-    apiDomain: baseUrl
-  });
-
-  userId = await supertokens.getUserId();
-
-  res = await fetch()
+  let res = await fetch(baseUrl + "/user/account");
+  if (res.ok) {
+    let account = (await res.json()).account;
+    let accountHTML = document.getElementById("account-name");
+    accountHTML.getElementsByTagName("a")[0].innerText = account.name;
+    accountHTML.style.display = "list-item";
+  } else {
+    document.getElementById("login").style.display = "list-item";
+    document.getElementById("registration").style.display = "list-item";
+  }
 }
 
-userInfo();
+window.addEventListener('load', userInfo);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { ChangePasswordDto } from "./dto/change-password.dto";
-import { ChangeEmailDto } from "./dto/change-email.dto";
+import { Injectable } from "@nestjs/common";
 import { ChangeAccountDto } from "./dto/change-account.dto";
 import { PrismaService } from "../prisma/prisma.service";
 import { Account, User } from "@prisma/client";
@@ -10,32 +8,15 @@ export class UserService {
   constructor(private prisma: PrismaService) {
   }
 
-  // async changePassword(changePasswordDto: ChangePasswordDto, userId: string): Promise<User> {
-  //   const user = await this.prisma.user.findUnique({ where: { id: userId } });
-  //   if (user.password === changePasswordDto.oldPassword) {
-  //     return await this.prisma.user.update({
-  //       where: { id: userId },
-  //       data: { password: changePasswordDto.newPassword }
-  //     });
-  //   }
-  //   throw new HttpException("Passwords are not same", HttpStatus.BAD_REQUEST);
-  // }
-  //
-  // async changeEmail(changeEmailDto: ChangeEmailDto, userId: string): Promise<User> {
-  //   const user = await this.prisma.user.findUnique({ where: { id: userId } });
-  //   return this.prisma.user.update({
-  //     where: { id: userId },
-  //     data: { email: changeEmailDto.newEmail }
-  //   });
-  // }
-
   async createUser(email: string, userId: string): Promise<User> {
     return this.prisma.user.create({
       data: {
         email: email,
         id: userId,
         account: {
-          create: {}
+          create: {
+            name: "User"
+          }
         }
       }
     });
