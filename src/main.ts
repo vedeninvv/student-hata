a1a80735c3dd399b1a0df1b1bfa0d449076da82b
Barabashka <vedeninvv@mail.ru>
2022-05-02 13:29:02 +0300

    lab6 fix: add authorize form to swagger

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
     .setTitle("Student Hata")
     .setDescription("A simple service for posting flats and searching for neighbors")
     .setVersion("1.0")
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
